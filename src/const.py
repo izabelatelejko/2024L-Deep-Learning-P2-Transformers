cc16c87d08d8e827f82e7b4e751b3fe948c72cb3
telejko <iza.telejko@gmail.com>
2024-04-13 23:42:23 +0200

    <3<3

---
+++ src/const.py
@@ -0,0 +1,7 @@
+"""Project constants."""
+
+MAIN_LABELS = ["yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"]
+AUDIO_PATH = "data/train/audio"
+BATCH_SIZE = 64
+VALIDATION_SPLIT = 0.2
+SEED = 123
---
+++ src/data_loader.py
@@ -0,0 +1,29 @@
+"""Module for loading the data."""
+
+import os
+
+import tensorflow as tf
+
+from src.const import AUDIO_PATH, MAIN_LABELS, BATCH_SIZE, VALIDATION_SPLIT, SEED
+
+
+def load_data():
+    """Load the data."""
+    labels_list = []
+
+    for class_dir in os.listdir(AUDIO_PATH):
+        labels_list += [
+            MAIN_LABELS.index(class_dir) if class_dir in MAIN_LABELS else 10
+            for _ in os.listdir(os.path.join(AUDIO_PATH, class_dir))
+        ]
+
+    train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+        directory=AUDIO_PATH,
+        labels=labels_list,
+        batch_size=BATCH_SIZE,
+        validation_split=VALIDATION_SPLIT,
+        seed=SEED,
+        output_sequence_length=16000,
+        subset="both",
+    )
+    return train_ds, val_ds

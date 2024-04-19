2d36f181d0417140d39560fc2ae6895848557654
telejkoi <iza.telejko@gmail.com>
2024-04-20 00:11:49 +0200

    improve load_and_preprocess and add transform_to_data_loader

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,8 @@
 """Module for data preprocessing and augmentation."""
 
 import tensorflow as tf
+import torch
+from torch.utils.data import DataLoader, TensorDataset
 
 from src.augmenter import generate_augmenter
 from src.data_loader import load_data
@@ -57,28 +59,31 @@ def preprocess_and_save(plot_samples: bool = False):
     del train_audio_with_augmented, train_labels_with_augmented
 
 
-def load_and_preprocess(plot_samples: bool = False):
+def load_and_preprocess(plot_samples: bool = False, augment_specs: bool = True):
     """Load augmented data, create binary labels, transform data to spectograms, and perform augmentation on spectograms."""
 
-    print("Load augmented data")
+    print("Loading augmented data...")
     train_audio_with_augmented, train_labels_with_augmented = load_augmented_data()
     _, val_ds = load_data()
+    print("Finished")
 
-    print("Create data with only main classes")
+    print("Creating data with only main classes...")
     train_audio_with_augmented_main = train_audio_with_augmented[
         train_labels_with_augmented != 10
     ]
     train_labels_with_augmented_main = train_labels_with_augmented[
         train_labels_with_augmented != 10
     ]
+    print("Finished")
 
-    print("Create binary dataset")
+    print("Creating binary dataset...")
     train_ds_binary = tf.data.Dataset.from_tensor_slices(
         (train_audio_with_augmented, create_binary_labels(train_labels_with_augmented))
     )
     del train_audio_with_augmented, train_labels_with_augmented
+    print("Finished")
 
-    print("Create main dataset")
+    print("Creating main dataset...")
     train_ds_main = tf.data.Dataset.from_tensor_slices(
         (train_audio_with_augmented_main, train_labels_with_augmented_main)
     )
@@ -88,16 +93,24 @@ def load_and_preprocess(plot_samples: bool = False):
     val_ds_main = val_ds
     val_ds_binary = val_ds.map(lambda x, y: (x, map_val_labels(y)))
     del val_ds
+    print("Finished")
 
-    print("Transform to spectograms")
+    print("Transforming audio data to spectograms...")
     train_ds_specs_binary = transform_to_spectograms(train_ds_binary)
     val_ds_specs_binary = transform_to_spectograms(val_ds_binary)
 
     train_ds_specs_main = transform_to_spectograms(train_ds_main)
     val_ds_specs_main = transform_to_spectograms(val_ds_main)
     del train_ds_main, train_ds_binary, val_ds_binary, val_ds_main
+    print("Finished")
 
-    print("Transform to numpy")
+    if augment_specs:
+        print("Augmenting spectograms...")
+        train_ds_specs_binary = augment_spectograms(train_ds_specs_binary)
+        train_ds_specs_main = augment_spectograms(train_ds_specs_main)
+        print("Finished")
+
+    print("Transforming data to numpy arrays...")
     train_ds_specs_binary_X, train_ds_specs_binary_y = dataset_to_np(
         train_ds_specs_binary
     )
@@ -109,12 +122,10 @@ def load_and_preprocess(plot_samples: bool = False):
 
     val_ds_specs_main_X, val_ds_specs_main_y = dataset_to_np(val_ds_specs_main)
     del val_ds_specs_main
+    print("Finished")
 
     val_ds_specs_main_X = val_ds_specs_main_X[val_ds_specs_main_y != 10]
     val_ds_specs_main_y = val_ds_specs_main_y[val_ds_specs_main_y != 10]
-
-    # print("Augment spectograms")
-    # train_ds_specs = augment_spectograms(train_ds_specs)
 
     # if plot_samples:
     #     plot_spectograms(train_ds_specs_binary)
@@ -129,3 +140,18 @@ def load_and_preprocess(plot_samples: bool = False):
         val_ds_specs_main_X,
         val_ds_specs_main_y,
     )
+
+
+def transform_to_data_loader(X, y, device):
+    """Transform numpy arrays to PyTorch DataLoader."""
+    X_t = torch.tensor(X, dtype=torch.float32).to(device)
+    y_t = torch.tensor(y, dtype=torch.float32).to(device)
+    del X, y
+
+    dataset = TensorDataset(X_t, y_t)
+    del X_t, y_t
+
+    data_loader = DataLoader(dataset, batch_size=256, shuffle=True)
+    del dataset
+
+    return data_loader

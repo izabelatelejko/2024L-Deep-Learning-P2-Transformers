92f25d7ba75366734484dbcdd399d39ad6063c08
telejkoi <iza.telejko@gmail.com>
2024-05-05 21:37:24 +0200

    update eval function

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,11 +4,11 @@ import numpy as np
 import torch
 
 
-def eval_acc_in_binary_task(model, X_test, y_test, device) -> float:
+def eval_acc_in_binary_task(model, X_test, y_test, dataset, device) -> float:
     X_test_tensor = torch.from_numpy(X_test)
     y_pred = (
         (1 * (model(X_test_tensor.float().to(device)) > 0.5)).cpu().numpy().squeeze()
     )
     acc = np.sum(y_pred == y_test) / y_pred.shape[0]
-    print(f"Test accuracy: {acc:.4f}")
+    print(f"{dataset} accuracy: {acc:.4f}")
     return y_pred, acc
